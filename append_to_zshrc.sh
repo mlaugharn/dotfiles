50ec64afe4c8f6c894fc312a5ae8cf4880edb877
Marc Laugharn <mlaugharn@gmail.com>
2022-06-11 21:33:13 -0700

    Create append_to_zshrc.sh

---
+++ append_to_zshrc.sh
@@ -0,0 +1,13 @@
+export PATH="$HOME/.local/bin:$PATH"
+
+# pyenv autoinstaller
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+
+# idiosyncrasy of installing fd with apt
+ln -s $(which fdfind) ~/.local/bin/fd
+alias fd=fdfind
+
+# fzf completions and ctrl-r support in zsh
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh  

d070bdc47d0035b2387fda8cb2931acf2e6be80e
Marc Laugharn <mlaugharn@gmail.com>
2023-07-29 14:42:39 -0700

    Create minimal.lua

---
+++ nvim/minimal.lua
@@ -0,0 +1,41 @@
+--[[
+notes:
+
+- use a nerdfont to get proper icon support
+- install universal-ctags, not ctags
+
+]]
+--
+
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.showtabline = 2
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.autoindent = true
+vim.o.ruler = true
+vim.o.showcmd = true
+vim.o.incsearch = true
+vim.o.so = 7
+vim.o.cmdheight = 2
+vim.o.selectmode = "mouse"
+vim.o.mouse = "a"
+vim.wo.foldcolumn = "1"
+-- vim.o.lazyredraw = true
+vim.o.showmatch = true
+vim.o.mat = 2
+vim.o.wildmenu = true
+vim.o.ai = true
+vim.o.si = true
+vim.o.expandtab = true
+vim.o.smarttab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.lbr = true
+vim.o.wrap = true
+vim.o.background = dark
+vim.o.encoding = "utf8"
+vim.o.laststatus = 2
+
+-- space as leader key
+vim.g.mapleader = " "
